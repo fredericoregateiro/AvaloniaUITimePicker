5ee27689bd28ffbc166e7b79f360bd39ce03a26e
Frederico Regateiro <frederico.regateiro@outlook.com>
2024-11-12 17:24:13 +0000

    first commit

---
+++ AvaloniaApplicationTimePicker/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaApplicationTimePicker.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
